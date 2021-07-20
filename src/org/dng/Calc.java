eb63c7a851a8c4dfc1f47b8327937900fb8ee4f8
Dmitry757 <glv11@mail.ru>
2021-07-21 00:26:49 +0300

    some code changed

--- src/org/dng/Calc.java
+++ src/org/dng/Calc.java
@@ -6,6 +6,7 @@ public class Calc {
     private String inValue;
     private double inNumber;
     private double currentNumber;
+    static MyDigits md=new MyDigits();
     static Scanner in = new Scanner(System.in );
 
 
@@ -13,19 +14,15 @@ public class Calc {
         System.out.println("tape something!");
         String str=in.next();
         in.close();
-
         System.out.println("you taped: "+str);
-        System.out.println("inputed value is Rome digit "+MyDigits.IsItRomeDigit(str));
+
+        //*** testing input value ****
+        System.out.println("inputed value is Rome digit = "+MyDigits.IsItRomeDigit(str));
+
+        System.out.println("search in HashSet... inputed value is Rome digit = "+MyDigits.IsItRomeDigitHash(str));
 
         return true;
     }
 
-    boolean testValue(String in){
-        char[] inValue = in.toCharArray();
-        for (char i :inValue) {
-
-        }
-        return true;
-    }
 
 }
--- src/org/dng/MyDigits.java
+++ src/org/dng/MyDigits.java
@@ -1,49 +1,60 @@
 package org.dng;
 
-import java.util.EnumSet;
+//import java.util.EnumSet;
 import java.util.HashSet;
 
-
-enum eromeDigits
+enum romeDigitsE
 {
-    I,V,X;
+    I,V,X,L;
 };
 
 public class MyDigits {
-//    static HashSet<String> romeDigits;
-//
+    static HashSet<String> romeDigits;
 //    static EnumSet<eromeDigits> romeDigits2;
-//
-//    public MyDigits() {
-//        romeDigits = new HashSet<>();
-//        romeDigits.add("I");
-//        romeDigits.add("V");
-//        romeDigits.add("X");
-//
-//        romeDigits2 = EnumSet.allOf(eromeDigits.class);
-//
-//    }
 
+    public MyDigits() {
+        romeDigits = new HashSet<>();
+        romeDigits.add("I");
+        romeDigits.add("V");
+        romeDigits.add("X");
+        romeDigits.add("L");
+
+//        romeDigits2 = EnumSet.allOf(eromeDigits.class);
+    }
 
 
     static boolean IsItRomeDigit (String val){
+        char[] inValue = val.toCharArray();
+        for (char c :inValue) {
 
-//        return romeDigits.contains(val);
-//        System.out.println(romeDigits2);
+            try {
+                romeDigitsE rd = romeDigitsE.valueOf( String.valueOf(c) );
+                System.out.println("rd=" + rd);
+            }
+            catch (IllegalArgumentException ex){
+                System.out.println("WTF?!");
+                return false;
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+                return false;
+            }
+        }
 
-        try {
-            eromeDigits rd = eromeDigits.valueOf(val);
-            System.out.println("rd=" + rd);
-            return true;
-        }
-        catch (IllegalArgumentException ex){
-            System.out.println("WTF?!");
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return false;
+        return true;
 
     }
+
+
+    static boolean IsItRomeDigitHash (String val){
+
+        char[] inValue = val.toCharArray();
+        for (char c :inValue) {
+            if (!romeDigits.contains( String.valueOf(c) )) return false;
+        }
+
+        return true;
+
+    }
+
 }
