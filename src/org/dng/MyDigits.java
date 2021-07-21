e0363db12fe0e5ff21938a880a7e4a2e282d4e77
Dmitry757 <glv11@mail.ru>
2021-07-22 01:57:20 +0300

    add method static boolean IsItRomeDigitM2 (String val) wich uses hashMap<String, Integer> instead of HashMap<Character, Integer> romeDigitsM in previous variant

--- src/org/dng/MyDigits.java
+++ src/org/dng/MyDigits.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 public class MyDigits {
     private static HashSet<Character> romeDigits;
     private static HashMap<Character, Integer> romeDigitsM;
+    private static HashMap<String, Integer> romeDigitsM2;
 
     private static HashSet<Character> arithmetical;
 
@@ -26,6 +27,27 @@ public class MyDigits {
         romeDigitsM.put('D',500);
         romeDigitsM.put('M',1000);
 
+        romeDigitsM2 = new HashMap<>();
+        romeDigitsM2.put("I",1);
+        romeDigitsM2.put("II",2);
+        romeDigitsM2.put("III",3);
+        romeDigitsM2.put("IV",4);
+        romeDigitsM2.put("V",5);
+        romeDigitsM2.put("VI",6);
+        romeDigitsM2.put("VII",7);
+        romeDigitsM2.put("VIII",8);
+        romeDigitsM2.put("IX",9);
+        romeDigitsM2.put("X",10);
+        romeDigitsM2.put("XI",11);
+        romeDigitsM2.put("XII",12);
+        romeDigitsM2.put("XIII",13);
+        romeDigitsM2.put("XIV",14);
+        romeDigitsM2.put("XV",15);
+
+        romeDigitsM2.put("L",50);
+        romeDigitsM2.put("C",100);
+        romeDigitsM2.put("D",500);
+        romeDigitsM2.put("M",1000);
 
         arithmetical = new HashSet<>();
         arithmetical.add(' ');
@@ -50,6 +72,14 @@ public class MyDigits {
         char[] inValue = val.toCharArray();
         for (char c : inValue) {
             if (!romeDigitsM.containsKey( Character.toUpperCase(c) ) & (!arithmetical.contains(c)) ) return false;
+        }
+        return true;
+    }
+
+    static boolean IsItRomeDigitM2 (String val){
+        char[] inValue = val.toCharArray();
+        for (char c : inValue) {
+            if (!romeDigitsM.containsKey( Character.toString(Character.toUpperCase(c)) ) & (!arithmetical.contains(c)) ) return false;
         }
         return true;
     }
